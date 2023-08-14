192b5fd3828b597aa38d3e5e6d9b0c25ee1cb274
Daouda Warme <dwarme99@gmail.com>
2023-08-14 15:46:08 +0200

    utils

---
+++ app/utils/constants-util.ts
@@ -0,0 +1,31 @@
+export const APP_MEDIA = {
+  image: {
+    homeHeroBackground:
+      'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/gray-background-08511157650368.jpg?v=1691507682',
+    profileAvatar:
+      'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/avatar_1664158844002.jpg?v=1691507682',
+    fullstackDev:
+      'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/fullstack-dev-0c5d08f2d4e7e3e8bc55711befad02f7.svg?v=1691507682',
+    skills:
+      'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/skills.png?v=1691507682',
+    works:
+      'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/works.png?v=1691507682',
+    appsLogo: {
+      cattown:
+        'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/cattown-logo.png?v=1691507682',
+      miniBzPay:
+        'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/minibank-logo.png?v=1691507682',
+      portfolio:
+        'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/portfolio-logo.png?v=1691507682',
+      iclofy:
+        'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/iclofy-logo.png?v=1691507682',
+      shop: 'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/shop-logo.png?v=1691507682',
+      github:
+        'https://cdn.shopify.com/s/files/1/0770/5690/1456/files/github-logo.png?v=1691507682',
+    },
+  },
+
+  video: {},
+};
+
+export const SESSION_SECRET = 'DY63GST6E-J-8b3@3S-84NDU!M-U@NR834kg3';
