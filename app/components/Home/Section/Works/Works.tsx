dd66c95e29b2c656cbf1edfdc596094cab8f64bb
Daouda Warme <dwarme99@gmail.com>
2023-10-14 13:50:51 +0200

    works image

--- app/components/Home/Section/Works/Works.tsx
+++ app/components/Home/Section/Works/Works.tsx
@@ -1,20 +1,26 @@
-import {APP_MEDIA} from '~/utils/constants-util';
-
 const Works: React.FC = () => {
   return (
     <div
       className="width-full display-flex justify-content-center"
       style={{height: '100%'}}
     >
-      <img
-        className="width-full"
-        style={{objectFit: 'contain'}}
-        alt="Works"
-        src={APP_MEDIA.image.works}
-        loading="lazy"
-        width={730}
-        height={460}
-      />
+      <picture>
+        <source
+          media="(max-width: 734px)"
+          srcSet="https://cdn.shopify.com/s/files/1/0770/5690/1456/files/built-small-theme-light.png?v=1697281784"
+        />
+        <source
+          media="(min-width: 735px)"
+          srcSet="https://cdn.shopify.com/s/files/1/0770/5690/1456/files/built-large-theme-ligth.png?v=1697282133"
+        />
+        <img
+          className="width-full"
+          style={{objectFit: 'contain'}}
+          alt="built"
+          loading="lazy"
+          src="https://cdn.shopify.com/s/files/1/0770/5690/1456/files/built-large-theme-ligth.png?v=1697282133"
+        />
+      </picture>
     </div>
   );
 };
