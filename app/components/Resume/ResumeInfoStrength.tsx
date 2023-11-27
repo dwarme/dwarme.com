c81b2f0325be21ecf21a2aa211e57aa330b01356
Daouda Warme <dwarme99@gmail.com>
2023-11-27 13:01:22 +0100

    updated

--- app/components/Resume/ResumeInfoStrength.tsx
+++ app/components/Resume/ResumeInfoStrength.tsx
@@ -5,26 +5,7 @@ function ResumeInfoStrength() {
     <ResumeInfo title="Strenghts">
       <div className="info-row">
         <div className="info-box text-box">
-          <p className="value">
-            The Apple Media Products Engineering team is one of the most
-            exciting examples of Apple’s long-held passion for combining art and
-            technology. These are the people who power the App Store, Apple TV,
-            Apple Music, Apple Podcasts, and Apple Books. And they do it on a
-            massive scale, meeting Apple’s high expectations with high
-            performance to deliver a huge variety of entertainment in over 35
-            languages to more than 150 countries These engineers build secure,
-            end-to-end solutions. They develop the custom software used to
-            process all the creative work, the tools that providers use to
-            deliver that media, all the server-side systems, and the APIs for
-            many Apple services. Thanks to Apple’s unique integration of
-            hardware, software, and services, engineers here partner to get
-            behind a single unified vision. That vision always includes a deep
-            commitment to strengthening Apple’s privacy policy, one of Apple’s
-            core values. Although services are a bigger part of Apple’s business
-            than ever before, these teams remain small, nimble, and
-            cross-functional, offering greater exposure to the array of
-            opportunities here.
-          </p>
+          <p className="value"></p>
         </div>
       </div>
     </ResumeInfo>
